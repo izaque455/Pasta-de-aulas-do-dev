523f305e7828c5c04f5d6c876725b287454860df
izaque455 <izaquepereiradesapereiradesa@gmail.com>
2025-07-21 15:33:32 -0300

    Alteração 1

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-setInterval(() => {
-console.log('oi')
-}, 1000);
+let number = 0
+let cron 
+let p = document.querySelector(".numero")
+function start(){
+    cron = setInterval(function(){
+        number++
+        p.innerHTML = number
+    }, 1)
+}
+function stop(){
+    clearInterval(cron)
+}
